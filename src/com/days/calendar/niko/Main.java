b87a06d856b7b9e8f3eede5707b6c0300bc9761c
Niko <git@storni.info>
2015-09-22 15:58:29 +0200

    example and comments

--- src/com/days/calendar/niko/Main.java
+++ src/com/days/calendar/niko/Main.java
@@ -4,13 +4,18 @@ import java.util.Calendar;
 
 public class Main
 {
-
+	// Usage example
+	/*
+	 * expected output on the 22nd of September 2015 in the afternoon:
+	 * 2018-5-17
+	 * Days between today and later: 998
+	 *
+	 */
 	public static void main(String[] args)
 	{
 		final Day today = new Day(Calendar.getInstance().getTime());
 		final Day later = today.dayIn(999);
 		System.out.println(later.getYear() + "-" + later.getMonth() + "-" + later.getDay());
 		System.out.println("Days between today and later: " + Day.DaysBetween(today, later));
-
 	}
 }
